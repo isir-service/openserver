6d7a040b8b4730d0d9ab8588d2d79cd050ccff09
isir <isir@192.168.0.102>
2022-10-12 23:09:20 +0800

    d

--- src/module/opdpdk/opdpdk.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OPDPDK_H__
-#define __OPDPDK_H__
-#include <pthread.h>
-#include "event.h"
-
-#include "opbox/oplist.h"
-
-
-#endif
--- src/module/opdpdk/suricate/suricate_main.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SURICATE_MAIN_H__
-#define __SURICATE_MAIN_H__
-
-int suriacte_run(int argc, char *argv[]);
-
-#endif
